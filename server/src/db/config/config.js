a64637166b0440530e4e655e71f05cabd898bd01
Ihor Ivliev <igor852745@gmail.com>
2020-02-11 11:16:16 +0200

    create config.js and install nodemon

---
+++ server/src/db/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    development:{
+        username:'ivliev',
+        password:'redrose',
+        database:'server_db',
+        host:'localhost',
+        port:5432,
+        dialect:'postgres'
+    },
+    test:{},
+    production:{}
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,3 @@
-const {User} = require('./db/models');
+import {User} from './db/models';
 
-function createUser(user) {
-
-}
-
-async function getUser(id) {
-
-    try {
-        const user = await User.findByPk(id);
-        if (user){
-            return user.get();
-        }else {
-            throw new Error('404 not found');
-        }
-    }catch (e) {
-        console.error(e);
-    }
-}
-
-function updateUser() {
-
-}
-
-function deleteUser() {
-
-}
-
-getUser(45).then(console.log).catch(console.error);
+User.findByPk(45).then(data =>{console.log(data.toJSON());});
